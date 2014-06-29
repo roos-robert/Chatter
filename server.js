7686e360ed9733eef5266f0f68fbe410458f0e31
rr222cy <rr222cy@student.lnu.se>
2014-06-29 18:47:17 +0200

    Communication with Socket.io

--- server.js
+++ server.js
@@ -5,9 +5,23 @@ mongo.connect('mongodb://127.0.0.1/chat', function(err, db) {
 	if (err) throw err;
 
 	client.on('connection', function(socket) {
-		// Wait for input
+
+		var col = db.collection('messages');
+
+		// Waiting for input
 		socket.on('input', function(data) {
-			console.log(data);
+			var name = data.name,
+				message = data.message,
+				whitespacePattern = /^\s*$/;
+
+			if(whitespacePattern.test(name) || whitespacePattern.test(message)) {
+				console.log('Both name and message must be given.');
+			}
+			else {
+				col.insert({name: name, message: message}, function() {
+					console.log("Data inserted to MongoDB");
+				});
+			}			
 		});
 	});
 });
