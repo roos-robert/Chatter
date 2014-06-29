658a41d3ff1433389809459bb763d235d410fda6
rr222cy <rr222cy@student.lnu.se>
2014-06-29 18:26:41 +0200

    Communication with Node server

--- server.js
+++ server.js
@@ -1,2 +1,13 @@
 var mongo = require('mongodb').MongoClient,
 	client = require('socket.io').listen(8080).sockets;
+
+mongo.connect('mongodb://127.0.0.1/chat', function(err, db) {
+	if (err) throw err;
+
+	client.on('connection', function(socket) {
+		// Wait for input
+		socket.on('input', function(data) {
+			console.log(data);
+		});
+	});
+});
